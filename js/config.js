3d9a42b11a0f8e7ced184b6b912751c3a74ec2ec
user1 <user1@gmail.com>
2025-05-21 18:46:57 +0530

    fifth commit

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_s5n8wDNMI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7npl59f25bl4gqtsklum1lcn3s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_BWBLxpEIZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5v97povgj77s6ii8rug5r0a0jc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
